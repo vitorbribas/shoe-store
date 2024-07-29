04b01d4de73cd012c641e8e103ca8c5985ff141b
Vítor Ribas <bsbribas66@gmail.com>
2024-07-29 17:08:08 -0300

    Setup dev and test gems

---
+++ inventory-manager/spec/rails_helper.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+require 'database_cleaner'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+RSpec.configure do |config|
+  config.fixture_paths = [
+    Rails.root.join('spec/fixtures')
+  ]
+
+  config.include FactoryBot::Syntax::Methods
+
+  config.use_transactional_fixtures = true
+
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+end
+
+RSpec.configure do |c|
+  c.around do |example|
+    DatabaseCleaner.strategy = example.metadata[:strategy] || :transaction
+    DatabaseCleaner.cleaning { example.run }
+  end
+
+  c.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
+end
