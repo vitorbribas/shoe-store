eef6ac38552f437f31f8c4570b681a715eb78c27
Vítor Ribas <bsbribas66@gmail.com>
2024-08-01 09:15:30 -0300

    Adds Mailhog mailer service

--- inventory-manager/config/environments/development.rb
+++ inventory-manager/config/environments/development.rb
@@ -28,6 +28,12 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch('MAILHOG_URL', 'localhost'),
+    port: 1025
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
